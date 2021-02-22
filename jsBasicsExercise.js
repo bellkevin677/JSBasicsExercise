c41103e4f5cf70163929ca5f21a7c7cbb0f0f742
bellkevin677 <bellkevin677@gmail.com>
2021-02-22 12:11:09 -0600

    complete

---
+++ jsBasicsExercise.js
@@ -0,0 +1,25 @@
+console.log("7");
+// Primitives, Booleans, Null and Undefinded, Stings, Operators
+// + - * / ** %
+console.log(5%4);
+console.log(6%5);
+console.log('I\'M MARY POPPINS, Y\'ALL!')
+console.log("Wanda" + "Vision");
+console.log("Kevin".length + "Bell".length);
+console.log("Avengers: Endgame."[16]);
+
+console.log("\\");
+console.log(5**4-9);
+console.log("I".length + "Am".length + "Iron".length + "Man".length - 10);
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+var m = 10;
+var c = 5;
+var u = m + c;
